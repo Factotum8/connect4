2fb5de35ec49a3dbcc6dd7baa27eea1344f7a401
a.amochkin <a.amochkin@integer-soft.ru>
2020-12-06 07:41:18 -0800

    Some refactoring

--- connect/connect.py
+++ connect/connect.py
@@ -135,24 +135,35 @@ class Game:
             return True
         return False
 
+    def play(self):
+        """
+        Game cycle
+        """
+        current_player = self.player_first
+        print('Player 1 will be X. Player 2 will be O.')
+
+        while True:
+            self.show_game_field()
+
+            choice = self.player_choice()
+            self.step(current_player, choice)
+
+            if self.check_winner(current_player):
+                print(f'The player {current_player} wins.')
+                self.show_game_field()
+                break
+
+            if not self.is_empty_cells_in_field():
+                print('Nobody wins. The board is filled up.')
+                break
+
+            current_player = self.player_second if current_player == self.player_first else self.player_first
+
+
+def main():
+    game = Game()
+    game.play()
+
 
 if __name__ == '__main__':
-    game = Game()
-
-    current_player = game.player_first
-    print('Player 1 will be X. Player 2 will be O.')
-
-    while not game.check_winner(current_player):
-        game.show_game_field()
-
-        choice = game.player_choice()
-        game.step(current_player, choice)
-
-        if not game.is_empty_cells_in_field():
-            print('Nobody wins. The board is filled up.')
-            break
-
-        current_player = game.player_second if current_player == game.player_first else game.player_first
-    else:
-        print(f'The player {current_player} wins.')
-        game.show_game_field()
+    main()
